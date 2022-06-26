37eeeae6eaf81b8c8466a3e888b59eb6b0177489
Alex <alex7100078@gmail.com>
2022-06-26 10:07:02 +0500

    Task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,18 +1,25 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    
+        
+   
+            
 
     // Задание №1.1. Оповещение о добавлении в корзину
+    
+    let message; // создайте переменную message
+        message = productName; // присвойте переменной message значение переменной productName
+        message = productName + " "; // добавьте в конец переменной message пробел
+        message = productName + " " + 'за';// добавьте в конец переменной message текст 'за'
+        message = productName + " " + 'за' + " ";// добавьте в конец переменной message пробел
+        message = productName + " " + 'за' + " " + productPrice;// добавьте в конец переменной message значение переменной productPrice
+        message = productName + " " + 'за' + " " + productPrice + " ";// добавьте в конец переменной message пробел
+        message = productName + " " + 'за' + " " + productPrice + " " + "теперь в корзине!";// добавьте в конец переменной message текст 'теперь в корзине!'
+    
+    console.log(message);// выведите переменную message в консоль
+  
 
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
 
     // Конец решения задания №1.1.
 
@@ -21,12 +28,10 @@ function addInCartMessage(title, price) {
 
 function addInCartCountChange(value) {
 
-    // Задание №1.2. Увеличение количества в корзине
-    let oldValue = value;
-
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
+    let oldValue = value; // Задание №1.2. Увеличение количества в корзине
+    let newValue; // создайте переменную newValue
+    newValue = oldValue; // присвойте ей значение oldValue
+    newValue + 1 // увеличьте значение newValue на 1
 
     // Конец решения задания №1.2.
 
