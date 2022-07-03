435a7e329cc259e7c5fc5bce468f68ef9a81c27b
Alex <alex7100078@gmail.com>
2022-07-03 18:02:32 +0500

    Переделал с использованием тренарных операторов

--- task_2/solution.js
+++ task_2/solution.js
@@ -7,15 +7,14 @@ function calcShipping(sum, min, shipping) {
 
     let shippingSum;// создайте переменную shippingSum
 
-    if(productsSum == 0) {// если productsSum равно 0,
-        shippingSum = 0;// то shippingSum присвоить значение 0
-    }
-   if(productsSum >= freeShippingMinSum) {// если productsSum Больше или равна freeShippingMinSum,
-       shippingSum = 0;// то shippingSum присвоить значение 0
-   }
-    if(productsSum > 0 && productsSum < freeShippingMinSum) {// если productsSum больше 0 и меньше freeShippingMinSum,
-        shippingSum = shippingPrice;// то shippingSum присвоить значение shippingPrice
-    }
+    // если productsSum равно 0,
+    // то shippingSum присвоить значение 0
+    // если productsSum Больше или равна freeShippingMinSum,
+    // то shippingSum присвоить значение 0
+    // если productsSum больше 0 и меньше freeShippingMinSum,
+    // то shippingSum присвоить значение shippingPrice,
+    shippingSum = productsSum == 0 || productsSum >= freeShippingMinSum ? 0 : shippingPrice; // Использовал тернарный оператор для выполнения всех условий,
+
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -28,13 +27,11 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
-    let discountSum;// создайте переменную discountSum
-
-    if(productsSum >= discountMinSum) {// если productsSum больше или равно discountMinSum,
-        discountSum = (discountPart / 100 * productsSum);// то присвойте discountSum значение discountPart процентов от productsSum,
-    } else { 
-        discountSum = 0;// иначе присвойте discountSum значение 0
-    }
+    // создайте переменную discountSum
+    // если productsSum больше или равно discountMinSum,
+    // то присвойте discountSum значение discountPart процентов от productsSum,
+    // иначе присвойте discountSum значение 0 
+    let discountSum = productsSum >= discountMinSum ? discountPart / 100 * productsSum : 0; // Использовал тернарный оператор для выполнения всех условий,
 
     // Конец решения задания №2.2.
 
