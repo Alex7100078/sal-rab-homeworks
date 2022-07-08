de7e6eefca7ec1333f0de0eab03f098a0428096c
Alex <alex7100078@gmail.com>
2022-07-09 00:41:18 +0500

    Task 4 Done

--- task_4/solution.js
+++ task_4/solution.js
@@ -23,7 +23,6 @@ function renderProductsCards(json) {
     clearProducts();
     products = parseProducts(json);
     length = products.length;
-
 for (let i = 0; i < length; i += 1) {
     addProduct(products[i]);
  }
